42c3ec5b89192698b57b3018f17b627ac477e6b5
dodoflix <dogukanmetan@gmail.com>
2022-04-19 15:42:50 +0300

    add file writing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import xlsxwriter
 
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -25,8 +26,7 @@ class Review:
         self.date = None
 
 
-# url = str(input("Lütfen URL'yi giriniz:"))
-url = "https://www.tripadvisor.com.tr/Hotel_Review-g297965-d6591460-Reviews-Sea_View_Otel_Kas-Kas_Turkish_Mediterranean_Coast.html"
+url = str(input("Lütfen URL'yi giriniz:"))
 if not urlControl(url):
     print("Hatalı veya eksik URL girdiniz!")
 else:
@@ -45,10 +45,11 @@ else:
         languageButton.click()
         actions.move_to_element(pageNumbers).perform()
         time.sleep(3)
+        title = driver.title
         reviewCount = driver.find_element(By.CLASS_NAME, "cdKMr.Mc._R.b").text
         reviewCount = int(reviewCount)
         reviewBox = driver.find_elements(By.CLASS_NAME, "cWwQK.MC.R2.Gi.z.Z.BB.dXjiy")
-        pageCount = reviewCount//len(reviewBox)
+        pageCount = reviewCount // len(reviewBox)
         reviewList = []
         nextButton = driver.find_element(By.CLASS_NAME, "ui_button.nav.next.primary")
         print("Yorum çekme işlemi başlatıldı!")
@@ -58,17 +59,37 @@ else:
             for review in reviewBox:
                 tempReview = Review()
                 tempReview.username = review.find_element(By.CLASS_NAME, "ui_header_link.bPvDb").text
-                tempReview.point = int(review.find_element(By.CLASS_NAME, "ui_bubble_rating").get_attribute("class").split("_")[-1])//10
+                tempReview.point = int(
+                    review.find_element(By.CLASS_NAME, "ui_bubble_rating").get_attribute("class").split("_")[-1]) // 10
                 tempReview.title = review.find_element(By.CLASS_NAME, "fCitC").text
                 tempReview.text = review.find_element(By.CLASS_NAME, "XllAv.H4._a").text
-                tempReview.date = review.find_element(By.CLASS_NAME, "euPKI._R.Me.S4.H3").text.replace(review.find_element(By.CLASS_NAME, "CrxzX").text, "")[1::]
+                tempReview.date = review.find_element(By.CLASS_NAME, "euPKI._R.Me.S4.H3").text.replace(
+                    review.find_element(By.CLASS_NAME, "CrxzX").text, "")[1::]
                 reviewList.append(tempReview)
             actions.move_to_element(nextButton).click()
             if page != pageCount - 1:
                 nextButton.click()
-                time.sleep(2)
+                time.sleep(3)
         print("Yorum çekme işlemi başarıyla tamamlandı.")
+        driver.close()
         print(len(reviewList), "adet yorum çekildi.")
+        workbook = xlsxwriter.Workbook(title)
+        worksheet = workbook.add_worksheet()
+        print(title, "adlı dosyaya yazılma işlemi başlatıldı.")
+        row = 0
+        for review in reviewList:
+            column = 0
+            worksheet.write(row, column, review.username)
+            column += 1
+            worksheet.write(row, column, review.date)
+            column += 1
+            worksheet.write(row, column, review.point)
+            column += 1
+            worksheet.write(row, column, review.title)
+            column += 1
+            worksheet.write(row, column, review.text)
+            row += 1
+        print("Dosyaya yazılma işlemi başarıyla tamamlandı.")
+        workbook.close()
     except NameError:
         print("Bir hata oldu!")
-    driver.close()
